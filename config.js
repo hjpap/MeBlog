a977612dcc2859ae368585dd42080ef665295ce9
hjpap <vibhjpap@gmail.com>
2014-04-07 10:58:22 +0800

    no message

--- config.js
+++ config.js
@@ -4,9 +4,7 @@
 exports.config = {
 
     siteInfo:{
-        //siteURL:"http://ricwme.duapp.com",
 		siteURL:"http://10.20.3.51:3000",
-        //siteStaticURL:"http://ricwme.duapp.com",
         siteStaticURL:"http://10.20.3.51:3000",
         title:"Me.",
         description:"Wei's Blog"
@@ -15,8 +13,7 @@ exports.config = {
     session_maxAge:30000,
 
     db: 'mongodb://10.20.3.51/MongoDB',
-	//db: 'mongodb://wei.wang:vib1234567@localhost/MongoDB',
-	//db:"mongodb://0Flba18fyozvD2enbIlcA1OR:yXWqzG60CS0NqslILRhlDBZlc7kTnEWg@mongo.duapp.com:8908/KeKkpbolakTqtFRuxXVX",
+
     articlePageSize:10
 
 }

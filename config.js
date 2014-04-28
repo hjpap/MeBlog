0d65b5f07ccc34cb895bcf09191771fd71931218
hjpap <vibhjpap@gmail.com>
2014-04-28 23:35:13 +0800

    no message

--- config.js
+++ config.js
@@ -4,15 +4,15 @@
 exports.config = {
 
     siteInfo:{
-		siteURL:"http://10.20.3.51:3000",
-        siteStaticURL:"http://10.20.3.51:3000",
+		siteURL:"http://localhost:3000",
+        siteStaticURL:"http://localhost:3000",
         title:"Me.",
         description:"Wei's Blog"
     },
     session_secret:"ricw",
     session_maxAge:30000,
 
-    db: 'mongodb://10.20.3.51/MongoDB',
+    db: 'mongodb://localhost/MongoDB',
 
     articlePageSize:10
 
